9f116cad59cb055cf9f326e6d8504922dc4d2431
Matheus Dias Pirixan <10965@adm.bancoamazonia.sa>
2021-12-03 13:31:35 -0300

    Add Quasar

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
 Vue.config.productionTip = false
 
 new Vue({
+  Quasar,
+  quasarUserOptions,
   router,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(App, { router, Quasar, quasarUserOptions })
---
+++ src/quasar-user-options.js
@@ -0,0 +1,173 @@
+import Vue from 'vue'
+
+import 'quasar/dist/quasar.css'
+import lang from 'quasar/lang/en/us.js'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import {
+  Quasar,
+  QLayout,
+  QHeader,
+  QFooter,
+  QMenu,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QItem,
+  QItemSection,
+  QItemLabel,
+  QInput,
+  QSeparator,
+  ClosePopup,
+  QTable,
+  QTh,
+  QTr,
+  QTd,
+  QCard,
+  QCardSection,
+  QCardActions,
+  QForm,
+  QBreadcrumbs,
+  QBreadcrumbsEl,
+  QTooltip,
+  QDialog,
+  QAvatar,
+  Notify,
+  QExpansionItem,
+  QCheckbox,
+  QSpace,
+  QSelect,
+  QPopupProxy,
+  QDate,
+  Dialog,
+  QBadge,
+  Loading,
+  QToggle,
+  QBtnDropdown,
+  QPageSticky,
+  QMarkupTable,
+  QChip,
+  QPopupEdit,
+  QField,
+  QSpinnerGears,
+  QRouteTab,
+  QTabs,
+  QTab,
+  QTabPanels,
+  QTabPanel,
+  QStepper,
+  QStep,
+  QStepperNavigation,
+  QScrollArea,
+  QTimeline,
+  QTimelineEntry,
+  LoadingBar,
+  QSplitter,
+  QSlideItem,
+  QBar,
+  Ripple
+} from 'quasar'
+Vue.use(Quasar, {
+  components: {
+    QLayout,
+    QHeader,
+    QFooter,
+    QMenu,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QItem,
+    QItemSection,
+    QItemLabel,
+    QInput,
+    QSeparator,
+    QTable,
+    QTh,
+    QTr,
+    QTd,
+    QCard,
+    QCardSection,
+    QCardActions,
+    QForm,
+    QBreadcrumbs,
+    QBreadcrumbsEl,
+    QTooltip,
+    QDialog,
+    QAvatar,
+    QExpansionItem,
+    QCheckbox,
+    QSpace,
+    QPopupProxy,
+    QDate,
+    QSelect,
+    QBadge,
+    QToggle,
+    QBtnDropdown,
+    QPageSticky,
+    QMarkupTable,
+    QChip,
+    QPopupEdit,
+    QField,
+    QRouteTab,
+    QTabs,
+    QTab,
+    QTabPanels,
+    QTabPanel,
+    QStepper,
+    QStep,
+    QStepperNavigation,
+    QScrollArea,
+    QTimeline,
+    QTimelineEntry,
+    QSplitter,
+    QSlideItem,
+    QBar,
+    Loading
+  },
+  directives: {
+    ClosePopup,
+    Ripple
+  },
+  plugins: {
+    Dialog,
+    QDialog,
+    Notify,
+    Loading,
+    QSpinnerGears,
+    LoadingBar
+  },
+  config: {
+    notify: {
+      position: 'top',
+      timeout: 2500
+    },
+    loading: { /* Loading defaults */ }
+  },
+  lang: lang
+})
+Notify.setDefaults({
+  position: 'top',
+  timeout: 2500
+})
+Loading.setDefaults({
+  message: '<strong>Carregando...</strong><br/><span class="text-accent"><strong>Aguarde</strong></span>',
+  spinner: QSpinnerGears,
+  spinnerColor: 'primary'
+
+})
+LoadingBar.setDefaults({
+  color: 'primary',
+  size: '10px',
+  position: 'top'
+})
